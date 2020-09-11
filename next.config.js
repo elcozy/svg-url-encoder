e30bddc40c42bbfafb0fb6494cded9b8e11da982
elcozy <maduforeynolds@yahoo.com>
2020-09-11 01:42:06 +0100

    config next

--- next.config.js
+++ next.config.js
@@ -1,23 +1,4 @@
-const withPlugins = require("next-compose-plugins");
-const optimizedImages = require("next-optimized-images");
-module.exports = withPlugins([
-  [
-    optimizedImages,
-    {
-      mozjpeg: {
-        quality: 80,
-      },
-      pngquant: {
-        speed: 3,
-        strip: true,
-        verbose: true,
-      },
-      imagesPublicPath: "/svg-url-encoder/_next/static/images/",
-    },
-  ],
-  {
-    assetPrefix: "/svg-url-encoder/",
-    basePath: "/svg-url-encoder",
-    env,
-  },
-]);
+module.exports = {
+  assetPrefix: "/svg-url-encoder/",
+  basePath: "/svg-url-encoder",
+};
